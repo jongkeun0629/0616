0f22cb575d05dea5b164132f351c18641106498a
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-16 15:30:24 +0900

    Anonymous Class(Calculator)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
-interface Greeting{
-    void greet();
+interface Calculator{
+    int compute(int a, int b);
 }
 
 public class Main {
     public static void main(String[] args) {
-        Greeting greeting = new Greeting() {
+        Calculator mul = new Calculator() {
             @Override
-            public void greet() {
-                System.out.println("Anonymous Class");
+            public int compute(int a, int b) {
+                return a * b;
             }
         };
+        System.out.println(mul.compute(3,4));
 
-        greeting.greet();
+        Calculator mulmul = new Calculator() {
+            @Override
+            public int compute(int a, int b) {
+                return (int) Math.pow(a * b, 2);
+            }
+        };
+        System.out.println(mulmul.compute(3,4));
+
+        Calculator connectNum = new Calculator() {
+            @Override
+            public int compute(int a, int b) {
+                return Integer.parseInt("" + a + b);
+            }
+        };
+        System.out.println(connectNum.compute(3, 4));
     }
 }
