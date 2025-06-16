0876ef31f02eb41e07451329ca91b56f0f2985ff
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-16 16:11:15 +0900

    CPU

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,73 @@
-interface Calculator{
-    int compute(int a, int b);
+class CPU {
+    public int register = 0;
+    public int[] memory = {5, 10, 0};
+
+    private static final int LOAD = 1;
+    private static final int SAVE = 2;
+    private static final int ADD = 3;
+    private static final int SUB = 4;
+    private static final int HALT = 5;
+
+    private int[][] program = {
+            { LOAD, 0 },         // register = memory[0]
+            { ADD, 1 },         // register = register + memory[1]
+            { SAVE, 2 },         // memory[2] = register
+            { HALT, 0 }         // stop program execution
+    };
+
+    public void executeProgram() {
+        int pc = 0;
+
+        while(true) {
+            // 실행할 명령어(load, save, add..)
+            int opcode = program[pc][0];
+            // 메모리 주소(memory[])
+            int operand = program[pc][1];
+
+            switch (opcode) {
+                case LOAD:
+                    register = memory[operand];
+                    System.out.println(
+                            "LOAD: Loading memory[" + operand + "] (" + memory[operand] + ") into register."
+                    );
+                    break;
+                case SAVE:
+                    memory[operand] = register;
+                    System.out.println(
+                            "SAVE: Saving memory[" + operand + "] (" + memory[operand] + ") from register."
+                    );
+                    break;
+                case ADD:
+                    register += memory[operand];
+                    System.out.println(
+                            "ADD: Adding memory[" + operand + "] (" + memory[operand] + ") to register. New register value: " + register
+                    );
+                    break;
+                case SUB:
+                    register -= memory[operand];
+                    System.out.println(
+                            "SUB: Subtracting memory[" + operand + "] (" + memory[operand] + ") to register. New register value: " + register
+                    );
+                    break;
+                case HALT:
+                    System.out.println("HALT: Stopping program execution.");
+                    return;
+                default:
+                    System.out.println("Unknown instruction. Halting.");
+                    return;
+            }
+
+            pc++;
+        }
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Calculator mul = new Calculator() {
-            @Override
-            public int compute(int a, int b) {
-                return a * b;
-            }
-        };
-        System.out.println(mul.compute(3,4));
+        CPU cpu = new CPU();
 
-        Calculator mulmul = new Calculator() {
-            @Override
-            public int compute(int a, int b) {
-                return (int) Math.pow(a * b, 2);
-            }
-        };
-        System.out.println(mulmul.compute(3,4));
+        cpu.executeProgram();
 
-        Calculator connectNum = new Calculator() {
-            @Override
-            public int compute(int a, int b) {
-                return Integer.parseInt("" + a + b);
-            }
-        };
-        System.out.println(connectNum.compute(3, 4));
+        System.out.println("Final memory state: memory[2] = " + cpu.memory[2]);
     }
 }
