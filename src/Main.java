05a950e3f0aaa8da076cd741ff502bf0f894b309
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-16 11:39:42 +0900

    Inner class2

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+class Car{
+    String brand;
+    Engine engine;
+
+    public Car(String brand){
+        this.brand = brand;
+        this.engine = new Engine();
+    }
+
+    class Engine{
+        public void start(){
+            System.out.println("The engine of " + brand + " is starting.");
+        }
+    }
+
+    public void startEngine(){
+        engine.start();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("자동차");
+
+        Car.Engine engine = car.new Engine();
+
+        car.startEngine();
+        car.engine.start();
+
+        engine.start();
+    }
+}
